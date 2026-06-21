7c0366f530e76c83ab2ce9e0c3a6bf93acd842a2
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-06-21 22:17:01 +0530

    adding js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let task= JSON.parse(localStorage.getItem("tasks")) || [];
+let tasks= JSON.parse(localStorage.getItem("tasks")) || [];
 
 displaytask();
+
+
+document.getElementById("taskinput").addEventListener("keypress", function(event){
+    if(event.key==="Enter"){
+        addtask();
+    }
+})
+
 
 function addtask(){
     let input=document.getElementById("taskinput");
@@ -12,13 +20,57 @@ function addtask(){
         return;
     }
 
-    task.push({
+    tasks.push({
         text:tasktext,
         done:false
     });
 
     savetask();
     input.value="";
+    input.focus();
     displaytask();
     
+}
+
+
+function displaytask(){
+    let tasklist=document.getElementById("tasklist");
+    tasklist.innerHTML="";
+
+    tasks.forEach((task,idx) => {
+        let li=document.createElement("li");
+        li.classList.add("task");
+        if(task.done){
+            li.classList.add("done")
+        }
+        li.innerHTML=`
+            <span>${task.text}</span>
+
+            <div class="action">
+                <button class="done-btn" onclick="toggletask(${idx})">✓</button>
+                <button class="delete-btn" onclick="deletetask(${idx})"> 🗑</button>
+            </div>
+            `;
+            tasklist.appendChild(li);
+    });
+}
+
+
+function toggletask(idx){
+    tasks[idx].done=!tasks[idx].done;
+    savetask();
+    displaytask();
+}
+
+
+
+function deletetask(idx){
+    tasks.splice(idx,1);
+    savetask();
+    displaytask();
+}
+
+
+function savetask(){
+    localStorage.setItem("tasks",JSON.stringify(tasks));
 }
